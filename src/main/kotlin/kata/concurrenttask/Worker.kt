364ae9d3d9507a7f72dd8f8b58bb4e28796fc4e8
Ilia Barancic <ilia.barancic@iits-consulting.de>
2023-05-05 09:09:23 +0200

    solution concurrent task

--- src/main/kotlin/kata/concurrenttask/Worker.kt
+++ src/main/kotlin/kata/concurrenttask/Worker.kt
@@ -1,11 +1,31 @@
 package kata.concurrenttask
 
-/**
- *  @see <a href="https://www.codewars.com/kata/5b34ca5791c746079c000067/java"> Concurrent Task Executor </a>
- */
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.time.Duration.Companion.seconds
+
 class Worker {
-    fun process(vararg tasks: Task, timeoutInSec: Int): ExecutedTasks {
-        return ExecutedTasks()
+    suspend fun process(vararg tasks: Task, timeoutInSec: Int): ExecutedTasks {
+        val executedTasks = ExecutedTasks()
+        withTimeoutOrNull(timeoutInSec.seconds.inWholeMilliseconds) {
+            tasks.forEach { task ->
+                launch {
+                    try {
+                        task.execute()
+                        executedTasks.addSuccessfull(task)
+                    } catch (e: CancellationException) {
+                        executedTasks.addTimedOut(task)
+                    } catch (e: Exception) {
+                        executedTasks.addFailed(task)
+                    }
+                }
+            }
+        }
+
+        return executedTasks
+
     }
 }
 
@@ -13,7 +33,31 @@ data class ExecutedTasks(
     val successfulTasks: MutableList<Task> = mutableListOf(),
     val timedOutTasks: MutableList<Task> = mutableListOf(),
     val failedTasks: MutableList<Task> = mutableListOf(),
-)
+) {
+    fun addSuccessfull(task: Task) = successfulTasks.add(task)
+    fun addFailed(task: Task) = failedTasks.add(task)
+    fun addTimedOut(task: Task) = timedOutTasks.add(task)
+}
 
-class Task(val name: String)
+
+sealed class Task(val name: String) {
+    abstract suspend fun execute()
+
+    override fun toString(): String {
+        return "Task(name='$name')"
+    }
+}
+
+class TimedTask(name: String, private val executionTimeInSec: Int) : Task(name) {
+    override suspend fun execute() {
+        delay(executionTimeInSec.seconds.inWholeMilliseconds)
+    }
+}
+
+class FailedTask(name: String) : Task(name) {
+    override suspend fun execute() {
+        throw RuntimeException("the disappointment is immeasurable")
+    }
+}
+
 
--- src/test/kotlin/kata/concurrenttask/WorkerTest.kt
+++ src/test/kotlin/kata/concurrenttask/WorkerTest.kt
@@ -1,7 +1,28 @@
 package kata.concurrenttask
 
+import io.kotest.matchers.collections.shouldContainExactly
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
 class WorkerTest {
 
     private val sut = Worker()
 
+    @Test
+    fun testSuccessful() {
+        val good = TimedTask("task1", 1)
+        runBlocking {
+            val result = sut.process(good, timeoutInSec = 5)
+            result.successfulTasks shouldContainExactly listOf(good)
+        }
+    }
+
+    @Test
+    fun testTimedOut() {
+        val timedOut = TimedTask("task1", 2)
+        runBlocking {
+            val result = sut.process(timedOut, timeoutInSec = 1)
+            result.timedOutTasks shouldContainExactly listOf(timedOut)
+        }
+    }
 }
